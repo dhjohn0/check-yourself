fcad7d403b01447672eb31ec2cbbb650f8666ca5
David Johnson <dhjohn0@gmail.com>
2017-08-10 21:13:42 -0500

    Basic form validation

---
+++ cy.ractive.js
@@ -0,0 +1,39 @@
+
+function ractiveCheckYourself(options) {
+  return Ractive.extend({
+    oninit: function () {
+      var validator = new CheckYourself(options);
+      var ractive = this;
+
+      ractive.observe('*', function (newVal, oldVal, path) {
+        if (path.indexOf('errors') === 0)
+          return;
+        if (typeof(oldVal) === 'undefined')
+          return;
+        var obj = {};
+        obj[path] = newVal;
+        var result = validator.check(obj, true);
+
+        var errors = _.merge(ractive.get('errors'), result.errors);
+        if (!_.find(errors, function (val) { return !!val; }))
+          errors = null;
+        ractive.set('errors', errors);
+      });
+
+      ractive.on('submit', function (e) {
+        var obj = ractive.get();
+        obj = _.omit(obj, 'errors');
+
+        var result = validator.check(obj);
+        if (result.failed) {
+          ractive.set('errors', result.errors);
+          e.event.preventDefault();
+        }else{
+          ractive.set('errors', null);
+          ractive.fire('submit-passed', e);
+        }
+      });
+    }
+  });
+  
+}
